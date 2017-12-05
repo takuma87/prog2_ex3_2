66573594bdfb571e819610eeea698468560edbec
Takuma Motohara <e175721@ie.u-ryukyu.ac.jp>
2017-12-05 11:25:46 +0900

    This is first trial.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import jp.ac.uryukyu.ie.ex3.pair18.Dice;
+
+public class Main{
+	public static void main (String[] args){
+        Dice dice = new Dice();
+    	dice.play();
+    	System.out.println(dice.getValue());
+    }
+}
---
+++ src/jp/ac/uryukyu/ie/ex3/pair18/Dice.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.ex3.pair18;
+
+public class Dice{
+    private int value;
+
+    public void Dice(){
+	    play();
+    }
+
+    public int getValue(){
+	return value;
+    }
+
+    public void setValue(int value){
+	this.value = value;
+    }
+
+    public void play(){
+	value = (int)(Math.random()*6) + 1;
+    }
+}
